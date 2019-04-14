0fbd7a97f8710f851e8aa0a98824620f2969b929
Alexandr Pankratiew <sasha.pankratiew@gmail.com>
2019-04-14 15:33:17 +0300

    Started job

---
+++ src/main/java/men/brakh/bsuirapi/AudsServlet.kt
@@ -0,0 +1,12 @@
+package men.brakh.bsuirapi
+
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class AudsServlet : HttpServlet() {
+    override fun doGet(req: HttpServletRequest?, resp: HttpServletResponse?) {
+
+        
+    }
+}
